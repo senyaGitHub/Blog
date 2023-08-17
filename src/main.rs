08054614aa8609789426d20d328dad57018d1522
senyaGitHub <knight.fo@yandex.ru>
2023-08-17 17:56:31 +0100

    Added boilerplate to test features on

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,26 @@
-use rand::Rng; 
+use text_io::read;
 
 fn main() {
-    let mut rng = rand::thread_rng();
-    let vals: Vec<u32> = (0..10).map(|_| rng.gen_range(0, 10)).collect();
-    println!("{:?}", vals);
+    let mut numbers = [0; 10];
+    for i in 0..10 {
+        print!("Num: ");
+        let num: i32 = read!();
+        numbers[i] = num;
+    }
+
+    // Bubble sort
+    for i in 0..9 {
+        for j in 0..(9 - i) {
+            if numbers[j] > numbers[j + 1] {
+                let temp = numbers[j];
+                numbers[j] = numbers[j + 1];
+                numbers[j + 1] = temp;
+            }
+        }
+    }
+    println!("{:?}", numbers);
+    println!("Sorted Numbers: {:?}", numbers);
 }
+
+  
+
