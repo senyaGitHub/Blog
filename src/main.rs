9df51bb7f45f01e90ebb98cabeae4304ad3dda7c
senyaGitHub <knight.fo@yandex.ru>
2023-08-17 19:32:37 +0100

    unitTest1

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,6 @@
 use text_io::read;
 
-fn main() {
-    let mut numbers = [0; 10];
-    for i in 0..10 {
-        print!("Num: ");
-        let num: i32 = read!();
-        numbers[i] = num;
-    }
-
-    // Bubble sort
+fn bubble_sort(mut numbers: [i32; 10]) -> [i32; 10]{
     for i in 0..9 {
         for j in 0..(9 - i) {
             if numbers[j] > numbers[j + 1] {
@@ -18,9 +10,33 @@ fn main() {
             }
         }
     }
-    println!("{:?}", numbers);
-    println!("Sorted Numbers: {:?}", numbers);
+    return numbers;
 }
 
+
+
+fn main() {
+    let mut numbers = [0; 10];
+    for i in 0..10 {
+        print!("Num: ");
+        let num: i32 = read!();
+        numbers[i] = num;
+    }
+    
+    println!("{:?}", numbers);
+    println!("Sorted Numbers: {:?}", bubble_sort(numbers));
+}
+
+
+
+
+#[cfg(test)]
+mod test{
+    use super::*;
+    #[test]
+    fn test_bubble_sort() {
+        assert_eq!(bubble_sort([10,9,8,7,6,5,4,3,2,1]) ,[1,2,3,4,5,6,7,8,9,10]);
+    }
+}
   
 
